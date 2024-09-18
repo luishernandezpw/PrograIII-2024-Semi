0fdb6b061f15acc885f59e957ce0f8291c43f419
Luis Hernandez <Estudiante@DESKTOP-J6D5H3A>
2024-09-18 08:05:05 -0600

    Terminacion del CRUD de alumnos con Python como backend

--- crud_alumno.py
+++ crud_alumno.py
@@ -7,9 +7,24 @@ class crud_alumno:
         return db.consultar("SELECT * FROM alumnos")
     
     def administrar(self, datos):
-        sql = """
-            INSERT INTO alumnos (codigo, nombre, direccion, telefono, email)
-            VALUES (%s, %s, %s, %s, %s) 
-        """
-        valores = (datos["codigo"], datos["nombre"], datos["direccion"], datos["telefono"], datos["correo"])
+        if(datos["accion"] == "nuevo"):	
+            sql = """
+                INSERT INTO alumnos (codigo, nombre, direccion, telefono, email)
+                VALUES (%s, %s, %s, %s, %s) 
+            """
+            valores = (datos["codigo"], datos["nombre"], datos["direccion"], datos["telefono"], datos["correo"])
+        elif(datos["accion"] == "modificar"):
+            sql = """
+                UPDATE alumnos
+                    SET codigo = %s, nombre = %s, direccion = %s, telefono = %s, email = %s
+                WHERE idAlumno = %s
+            """
+            valores = (datos["codigo"], datos["nombre"], datos["direccion"], 
+                       datos["telefono"], datos["correo"], datos["idAlumno"])
+        elif(datos["accion"] == "eliminar"):
+            sql = """
+                DELETE FROM alumnos
+                WHERE idAlumno = %s
+            """
+            valores = (datos["idAlumno"],)
         return db.procesar_consultas(sql, valores)
