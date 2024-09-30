b9b3d16ac00494f2677dbbfbbb5faf77b06749f3
Luis Hernandez <sarco_luis@hotmail.com>
2024-09-30 11:48:51 -0600

    Terminacion del sistema academico con alumnos, doncentes y materias

--- server.py
+++ server.py
@@ -3,8 +3,13 @@ from urllib.parse import urlparse, parse_qs
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import json
 import crud_alumno
+import crud_docente
+import crud_materia
 
 crudAlumno = crud_alumno.crud_alumno()
+crudDocente = crud_docente.crud_docente()
+crudMateria = crud_materia.crud_materia()
+
 class servidorBasico(SimpleHTTPRequestHandler):
     def do_GET(self):
         url_parseada = urlparse(self.path)
@@ -23,14 +28,32 @@ class servidorBasico(SimpleHTTPRequestHandler):
             self.send_response(200)
             self.end_headers()
             self.wfile.write(json.dumps(crudAlumno.consultar()).encode('utf-8'))
+
+        elif path == '/docentes':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(json.dumps(crudDocente.consultar()).encode('utf-8'))
+        
+        elif path == '/materias':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(json.dumps(crudMateria.consultar()).encode('utf-8'))
     def do_POST(self):
         longitud = int(self.headers['Content-Length'])
         body = self.rfile.read(longitud)
         datos = body.decode('utf-8')
         datos = parse.unquote(datos)
         datos = json.loads(datos)
-        resp = {"msg": crudAlumno.administrar(datos)}
+        
+        if self.path == '/alumnos':   
+            resp = {"msg": crudAlumno.administrar(datos)}
+            
+        elif self.path == '/docentes':
+            resp = {"msg": crudDocente.administrar(datos)}
 
+        elif self.path == '/materias':
+            resp = {"msg": crudMateria.administrar(datos)}
+            
         self.send_response(200)
         self.end_headers()
         self.wfile.write(json.dumps(resp).encode('utf-8'))
